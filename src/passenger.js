a4c80750a6fabb6eba223b43d9de998e1c9d91bd
Yongki Kim <ygflove@likelion.org>
2023-09-22 01:46:47 +0900

    :fire: 상수 관리 모듈 제거

--- src/passenger.js
+++ src/passenger.js
@@ -1,14 +1,18 @@
-const { MAX_FLOOR, MIN_FLOOR } = require("./constants");
-
-function getRandomNumber() {
-  return Math.floor(Math.random() * (MAX_FLOOR - MIN_FLOOR + 1) + MIN_FLOOR);
-}
-
 class Passenger {
+  static MAX_FLOOR = 10;
+  static MIN_FLOOR = 1;
+
   constructor({ currentFloor, targetFloor }) {
     this.currentFloor = currentFloor || getRandomNumber();
     this.targetFloor = targetFloor || getRandomNumber();
     this.direction = Math.sign(this.targetFloor - this.currentFloor);
+  }
+
+  getRandomNumber() {
+    return Math.floor(
+      Math.random() * (Passenger.MAX_FLOOR - Passenger.MIN_FLOOR + 1) +
+        Passenger.MIN_FLOOR
+    );
   }
 }
 
